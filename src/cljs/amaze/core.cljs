8af4ee4aed47b3763e2e9b37936c29408fed545b
Scott Brown <brown131@yahoo.com>
2021-03-21 16:05:58 -0500

    added code comments

--- src/cljs/amaze/core.cljs
+++ src/cljs/amaze/core.cljs
@@ -14,27 +14,34 @@
   (when debug?
     (println "dev mode")))
 
+;; HTML5 drawing canvas.
 (def monet-canvas (atom nil))
 
+;; State of the maze UI controls.
 (def maze-state {:width (reagent/atom "20")
                  :height (reagent/atom "20")
                  :thickness (reagent/atom "10")
                  :breadth (reagent/atom "15")})
 
+;; Facts database holding the generated maze cells.
 (def maze-cells (atom empty-db))
 
+;; Define and set the arity of the db relations:
 (db-rel direction ^:index d p)
 (db-rel maze ^:index p d)
 
+;; Maze directions with coordinate deltas.
 (def directions (db [direction :north [0 -1]]
                     [direction :south [0 1]]
                     [direction :east [1 0]]
                     [direction :west [-1 0]]))
 
+(defn get-maze-state [kw] (js/parseInt @(kw maze-state)))
+
 (defn render-cell "Display a cell at the specified coordinates."
   [x y dir]
-  (let [thickness (js/parseInt @(:thickness maze-state))
-        breadth (js/parseInt @(:breadth maze-state))
+  (let [thickness (get-maze-state :thickness)
+        breadth (get-maze-state :breadth)
         [dx dy] (first (run-db 1 directions [p] (direction dir p)))
         px (- (+ (* x (+ breadth thickness)) (* (Math/min dx 0) thickness)) breadth)
         py (- (+ (* y (+ breadth thickness)) (* (Math/min dy 0) thickness)) breadth)
@@ -42,14 +49,16 @@
         ch (+ breadth (* thickness (Math/abs dy)))]
     (canvas/add-entity @monet-canvas [px py]
                        (canvas/entity {:x px :y py :w cw :h ch} nil
-                                      (fn [ctx val] (-> ctx (canvas/fill-style :white) (canvas/fill-rect val)))))))
+                                      (fn [ctx val] (-> ctx
+                                                        (canvas/fill-style :white)
+                                                        (canvas/fill-rect val)))))))
 
 (defn calc-canvas-size "Calculate the size in pixels of the maze background."
   []
-  (let [width (js/parseInt @(:width maze-state))
-        height (js/parseInt @(:height maze-state))
-        thickness (js/parseInt @(:thickness maze-state))
-        breadth (js/parseInt @(:breadth maze-state))
+  (let [width (get-maze-state :width)
+        height (get-maze-state :height)
+        thickness (get-maze-state :thickness)
+        breadth (get-maze-state :breadth)
         canvas-width (+ (* width (+ thickness breadth)) thickness)
         canvas-height (+ (* height (+ thickness breadth)) thickness)]
     [canvas-width canvas-height]))
@@ -57,7 +66,8 @@
 (defn render-canvas []
   (let [[canvas-width canvas-height] (calc-canvas-size)]
     [box :width (str canvas-width "px") :height (str canvas-height "px")
-     :child [:canvas {:id "canvas" :width (str canvas-width) :height (str canvas-height) :style {:background-color :black}}]]))
+     :child [:canvas {:id "canvas" :width (str canvas-width) :height (str canvas-height)
+                      :style {:background-color :black}}]]))
 
 (defn clear-canvas []
   (canvas/clear! @monet-canvas)
@@ -82,8 +92,8 @@
 (defn unvisited "Check if a cell is not yet part of the maze."
   [from-x from-y to-direction]
   (let [[to-x to-y] (neighbor-cell from-x from-y to-direction)]
-    (and (<= 1 to-x (js/parseInt @(:width maze-state)))
-         (<= 1 to-y (js/parseInt @(:height maze-state)))
+    (and (<= 1 to-x (get-maze-state :width))
+         (<= 1 to-y (get-maze-state :height))
          (empty? (cl/run-db 1 @maze-cells [d] (maze [to-x to-y] d))))))
 
 (defn generate-maze [from-x from-y]         
@@ -93,19 +103,19 @@
             [to-x to-y] (neighbor-cell from-x from-y to-direction)
             from-direction (opposite-direction to-direction)]
         (render-cell to-x to-y from-direction)
-        (reset! maze-cells (db-fact @maze-cells maze [to-x to-y] from-direction))
+        (swap! maze-cells #(db-fact % maze [to-x to-y] from-direction))
         (generate-maze to-x to-y)
         (generate-maze from-x from-y)))))
 
 (defn render-exit []
-  (let [x (js/parseInt @(:width maze-state))
-        y (inc (rand-int (js/parseInt @(:height maze-state))))]
-    (reset! maze-cells (db-fact @maze-cells maze [x y] :east))
+  (let [x (get-maze-state :width)
+        y (inc (rand-int (get-maze-state :height)))]
+    (swap! maze-cells #(db-fact % maze [x y] :east))
     (render-cell x y :east)))
     
 (defn render-maze []
   (clear-canvas)
-  (generate-maze 0 (inc (rand-int (js/parseInt @(:height maze-state)))))
+  (generate-maze 0 (inc (rand-int (get-maze-state :height))))
   (render-exit))
 
 (defn print-maze []
@@ -116,20 +126,25 @@
 (defn main-panel []
   [v-box :src (at) :class "container" :children
    [[box :align :center :child [title :src (at) :label "AMAZE!" :level :level1
-                                :style {:font-family ["Courier New", "monospace"] :font-weight :bold :color :orange}]]
+                                :style {:font-family ["Courier New", "monospace"]
+                                        :font-weight :bold :color :orange}]]
     [h-box :children
      [[v-box :size "1" :children
        [[label :label "Maze Width"]
-        [input-text :width "90px" :model (:width maze-state) :on-change #(reset! (:width maze-state) %)]]]
+        [input-text :width "90px" :model (:width maze-state)
+         :on-change #(reset! (:width maze-state) %)]]]
       [v-box :size "1" :children
        [[label :label "Maze Height"]
-        [input-text :width "90px" :model (:height maze-state) :on-change #(reset! (:height maze-state) %)]]]
+        [input-text :width "90px" :model (:height maze-state)
+         :on-change #(reset! (:height maze-state) %)]]]
       [v-box :size "1" :children
        [[label :label "Wall Thickness"]
-        [input-text :width "90px" :model (:thickness maze-state) :on-change #(reset! (:thickness maze-state) %)]]]
+        [input-text :width "90px" :model (:thickness maze-state)
+         :on-change #(reset! (:thickness maze-state) %)]]]
       [v-box :size "7" :children
        [[label :label "Hall Breadth"]
-        [input-text :width "90px" :model (:breadth maze-state) :on-change #(reset! (:breadth maze-state) %)]]]]]
+        [input-text :width "90px" :model (:breadth maze-state)
+         :on-change #(reset! (:breadth maze-state) %)]]]]]
     [:p]
     [h-box :children
      [[box :child [button :label "Generate" :on-click render-maze :class "btn-primary"]]
